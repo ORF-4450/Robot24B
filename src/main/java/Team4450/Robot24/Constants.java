51e78d783c124ba586655c1284c9c7bd4b503d13
Cole Wilson <cole@colewilson.xyz>
2024-02-04 12:41:47 -0800

    add todo comment about ratios

--- src/main/java/Team4450/Robot24/Constants.java
+++ src/main/java/Team4450/Robot24/Constants.java
@@ -51,7 +51,7 @@ public final class Constants
     public static final double  SHOOTER_FEED_SPEED = 1;
 
     // gear ratio of revolutions of shaft to revolutions of assembly
-    public static final double  SHOOTER_PIVOT_FACTOR = 0.1;
+    public static final double  SHOOTER_PIVOT_FACTOR = 0.1; //TODO: add correct gear ratios
 
     
 
